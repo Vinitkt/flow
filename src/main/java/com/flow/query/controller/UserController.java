ab39f369050e1ca63161872529c9c41a153d6d7b
vinit <vinit.tiwari@thrymr.net>
2018-01-13 15:39:01 +0530

    test commit

--- src/main/java/com/flow/query/controller/UserController.java
+++ src/main/java/com/flow/query/controller/UserController.java
@@ -39,6 +39,7 @@ public class UserController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "welcome to Thrymr Software PVT Ltd Hyderabad Gachibowli Kapil Towers Financial District" ;
+		return "welcome to Thrymr Software PVT Ltd Hyderabad Gachibowli Kapil Towers Financial District"
+				+ "telanga India" ;
 	}
 }
