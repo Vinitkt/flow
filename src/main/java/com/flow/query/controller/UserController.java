6a7de2b341efea2b0651bb849024dd2c7b35de79
vinit <vinit.tiwari@thrymr.net>
2018-01-13 16:42:14 +0530

    test commit

--- src/main/java/com/flow/query/controller/UserController.java
+++ src/main/java/com/flow/query/controller/UserController.java
@@ -39,6 +39,6 @@ public class UserController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "welcome to Thrymr Software PVT Ltd Hyderabad Gachibowli " ;
+		return "welcome to Thrymr Software PVT Ltd Hyderabad Gachibowli   " ;
 	}
 }
