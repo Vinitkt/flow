52450240d8a497fd41893613e6d9a48f6e7c4c1e
vinit <vinit.tiwari@thrymr.net>
2018-01-13 16:13:30 +0530

    test commit

--- src/main/java/com/flow/query/controller/UserController.java
+++ src/main/java/com/flow/query/controller/UserController.java
@@ -39,6 +39,6 @@ public class UserController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "welcome to Thrymr Software PVT Ltd Hyderabad Gachibowli Kapil Towers Financial District  " ;
+		return "welcome to Thrymr Software PVT Ltd Hyderabad Gachibowli Kapil Towers Financial District This is located in India " ;
 	}
 }
